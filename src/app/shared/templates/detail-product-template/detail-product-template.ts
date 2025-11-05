8416b14926d441ad61ffd83b896329583ac0be4c
Lumarka <luisamarkarian@gmail.com>
2025-11-04 22:05:47 -0300

    Creacion del componente product-detail

--- src/app/shared/templates/detail-product-template/detail-product-template.ts
+++ src/app/shared/templates/detail-product-template/detail-product-template.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ProductDetail } from '../../organisms/product-detail/product-detail';
 
 @Component({
   selector: 'app-detail-product-template',
-  imports: [],
+  imports: [ProductDetail],
   templateUrl: './detail-product-template.html',
   styleUrl: './detail-product-template.scss'
 })
