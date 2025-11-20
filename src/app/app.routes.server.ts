faac4e3a6f1b904aff64f201899777f6db522e47
jhonpulido1990 <3825@holbertonstudents.com>
2025-11-20 14:56:24 -0500

    feat(config): actualizar límites de tamaño de presupuesto en angular.json y agregar ruta de detalles en app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'details/:id',
+    renderMode: RenderMode.Server, // Usar renderizado del lado del servidor
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
