0005d3abd46dd04ca43efc9bf502a5302e221d80
jhonpulido1990 <3825@holbertonstudents.com>
2025-11-21 10:54:25 -0500

    feat(environment): actualizar URL de la API para desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://maa-hairstudio-backend-0hww.onrender.com/api/v1/' // URL de la API para desarrollo
+  apiUrl: 'http://localhost:3000/api/v1/' // URL de la API para desarrollo
 };
