3606041d2f7ccca3c9e45e758caad10e6192b651
Lumarka <luisamarkarian@gmail.com>
2025-12-09 22:12:12 -0300

    fix: uncomment details route and update render mode for catch-all route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,7 +1,7 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-/*   {
+  {
     path: 'details/:id',
     renderMode: RenderMode.Server, // Usar renderizado del lado del servidor
   },
@@ -12,9 +12,9 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: 'services',
     renderMode: RenderMode.Server, // Usar renderizado del lado del servidor
-  }, */
+  },
   {
     path: '**',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender, // Usar prerenderizado para todas las demás rutas
   }
 ];
