21b2f486e12629ea367ff1912d2400cb7daa592c
BelenFrak <belenfrak@gmail.com>
2026-01-07 17:44:50 -0300

    hola

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1/'
+  apiUrl: 'https://maa-hairstudio-rboo.onrender.com/api/v1/'
 };
 /* apiUrl: 'https://maa-hairstudio-rboo.onrender.com/api/v1/' */
